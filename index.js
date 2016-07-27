606ee74e173624cd06faec5ee9a96970e2e16ba3
Marten Schilstra <mail@martenschilstra.nl>
2016-07-27 13:57:51 +0200

    Import the Shopify Buy SDK

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-shopify-buy'
+  name: 'ember-shopify-buy',
+
+  init: function() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    this.treePaths['vendor'] = 'node_modules';
+  },
+
+  included: function(app) {
+    app.import('vendor/shopify-buy/dist/shopify-buy.polyfilled.amd.js');
+  }
 };
